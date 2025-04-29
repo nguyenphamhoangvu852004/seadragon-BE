972775e603bf9dd1af0ff0ba540c708d732a15fc
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-04-29 15:24:52 +0700

    controller thêm, sửa sản phẩm

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,3 +1,5 @@
+import { CreateProductDTO } from './dto/create.product.dto'
+import { UpdateProductDTO } from './dto/update.product.dto'
 import { IProductService } from './service/IProductService'
 import { Request, Response } from 'express'
 export default class ProductController {
@@ -68,12 +70,39 @@ export default class ProductController {
   }
 
   async createProduct(req: Request, res: Response) {
-    const { name, description, price, categoryId } = req.body
-    const image = req.file?.filename
-    const product = await this.productService.createProduct(data)
+    const { title, description, price, categoryId } = req.body
+    const image = req.file?.filename as string
+    const newProduct = new CreateProductDTO()
+    newProduct.title = title
+    newProduct.description = description
+    newProduct.price = price as number
+    newProduct.categoryId = categoryId as number
+    newProduct.image = image
+    console.log('newProduct', newProduct)
+    const product = await this.productService.createProduct(newProduct)
     res.status(200).json({
       status: 200,
       message: 'Create product successfully',
+      data: product // Replace with actual data from your service
+    })
+  }
+  async updateProduct(req: Request, res: Response) {
+    const id = req.params.id
+    const { title, description, price, categoryId } = req.body
+    console.log('body', req.body)
+    const image = req.file?.filename as string
+    const newProduct = new UpdateProductDTO()
+    newProduct.id = id
+    newProduct.title = title
+    newProduct.description = description
+    newProduct.price = price as number
+    newProduct.categoryId = categoryId as number
+    newProduct.image = image
+    console.log('newProduct', newProduct)
+    const product = await this.productService.updateProduct(newProduct)
+    res.status(200).json({
+      status: 200,
+      message: 'Update product successfully',
       data: product // Replace with actual data from your service
     })
   }
