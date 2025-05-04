13739ff986d4d8276613cd2ec5aee76ac398f1fe
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-04 22:19:51 +0700

    add route customer account auth

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
-import { authRouter } from './auth.route'
 import { categoryRouter } from './category.route'
 import { productRouter } from './product.route'
-
+import { authRouter } from './auth.route'
+import { acccountRouter } from './account.router'
+import { customerRouter } from './customer.route'
 const router = Router()
 
-router.use('/auth', authRouter)
+// router.use('/auth', authRouter)
 router.use('/categories', categoryRouter)
 router.use('/products', productRouter)
+router.use('/auth', authRouter)
+router.use('/accounts', acccountRouter)
+router.use('/customers', customerRouter)
 export const v1Router = router
