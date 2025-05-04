805b8fac1cc817a04fe7aeef8225c2c9a7a31c02
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-04 22:17:40 +0700

    add interface category service

--- src/modules/category/service/ICategoryService.ts
+++ src/modules/category/service/ICategoryService.ts
@@ -4,5 +4,7 @@ export interface ICategoryService {
   getCategoryById(id: string): Promise<any>
   createCategory(data: any): Promise<any>
   updateCategory(data: any): Promise<any>
-  deleteCategory(id: string): Promise<any>
+  deleteCategory(ids: number[]): Promise<any>
+  deleteTemporaryCategory(ids: number[]): Promise<any>
+  restoreTemporaryCategory(id: number): Promise<any>
 }
