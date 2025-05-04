2f7622ba18406401a31e1dc2641feec2c6217a4d
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-04 22:14:28 +0700

    add interface role service

---
+++ src/modules/role/service/IRoleService.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default interface IRoleService {
+  createRole(data: any): Promise<any>
+  getRoleById(id: string): Promise<any>
+  getAllRoles(): Promise<any[]>
+  updateRole(id: string, data: any): Promise<any>
+  deleteRole(id: string): Promise<void>
+}
