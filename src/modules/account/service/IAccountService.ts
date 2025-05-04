4c4a4b37e31a238d5388c42ae4a342e121e6331a
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-04 22:10:30 +0700

    add interface account service file

---
+++ src/modules/account/service/IAccountService.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface IAccountService {
+  createAccount(data: any): Promise<any>
+  login(data: any): Promise<any>
+  getAccount(data: any): Promise<any>
+  getListAccount(): Promise<any>
+}
