f20a70d49d31cae3b515636e75e942a38b52fc0f
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-05 10:36:54 +0700

    thêm method

--- src/modules/customer/service/ICustomerService.ts
+++ src/modules/customer/service/ICustomerService.ts
@@ -3,9 +3,11 @@ import { Customers } from '../../../entities/customers.entity'
 
 export default interface ICustomerService {
   getList(): Promise<Customers[]>
+  getAllDeletedTemporaryCustomers(): Promise<Customers[]>
   createCustomer(data: any): Promise<Customers>
   deleteTemporaryCustomers(ids: number[]): Promise<Customers[]>
-  restoreTemporaryService(id: number): Promise<Customers>
+  restoreTemporaryCustomer(id: number): Promise<Customers>
   getDetailCustomer(id: number): Promise<Customers>
   deleteCustomers(ids: number[]): Promise<Customers[]>
+
 }
