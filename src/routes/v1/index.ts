c84743f69cb97552b49b53f0ab34efb2e5c586db
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-05 14:42:08 +0700

    them route blogs

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 import { categoryRouter } from './category.route'
 import { productRouter } from './product.route'
 import { authRouter } from './auth.route'
-import { acccountRouter } from './account.router'
+import { acccountRouter } from './account.route'
 import { customerRouter } from './customer.route'
+import { blogRouter } from './blog.route'
 const router = Router()
 
 // router.use('/auth', authRouter)
@@ -12,4 +13,5 @@ router.use('/products', productRouter)
 router.use('/auth', authRouter)
 router.use('/accounts', acccountRouter)
 router.use('/customers', customerRouter)
+router.use('/blogs',blogRouter)
 export const v1Router = router
