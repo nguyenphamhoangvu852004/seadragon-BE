3624531644528a1be002854c2f08761f473665f4
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-05 16:57:32 +0700

    thêm xoá mềm, lấy ds đã xoá mềm, phục hồi,cập nhật blog,

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -3,7 +3,8 @@ import { IBlogService } from './service/IBlogService'
 import { classToPlain } from 'class-transformer'
 import { CreateBlogInputDTO } from './dto/create.blog.dto'
 import { NotFoundException } from '../../shared/NotFound.exeception'
-import { BadRequestException } from '../../shared/BadRequest.exeception'
+import { UpdateBlogInputDto } from './dto/update.blog.dto'
+import { log } from 'console'
 
 export default class BlogController {
   blogService: IBlogService
@@ -12,12 +13,14 @@ export default class BlogController {
   }
   async createBlog(req: Request, res: Response) {
     try {
-      const { id, title, body, image } = req.body
+      const { userId, title, body } = req.body
+      const image = req.file?.filename as string
       const newDto = new CreateBlogInputDTO()
-      newDto.userId = id
+      newDto.userId = userId
       newDto.title = title
       newDto.image = image
       newDto.body = body
+      log(newDto.image)
       const response = await this.blogService.createBlog(newDto)
       res.status(201).json({
         status: 201,
@@ -84,6 +87,95 @@ export default class BlogController {
         data: {}
       })
       return
+    }
+  }
+  async deleteTemporaryBlogs(req: Request, res: Response) {
+    const { ids } = req.body
+    try {
+      const response = await this.blogService.deleteTemporaryBlogs(ids)
+      res.status(200).json({
+        status: 200,
+        message: 'Delete blog successfully',
+        data: classToPlain(response)
+      })
+      return
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({
+          status: 400,
+          message: error.message,
+          data: {}
+        })
+        return
+      }
+    }
+  }
+  async restoreTemporaryBlogs(req: Request, res: Response) {
+    try {
+      const { ids } = req.body
+      const list = await this.blogService.restoreTemporaryBlogs(ids)
+      res.status(200).json({
+        status: 200,
+        message: 'Restore blog successfully',
+        data: classToPlain(list)
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({
+          status: 400,
+          message: error.message,
+          data: {}
+        })
+        return
+      }
+    }
+  }
+  async getAllTemporaryBlogs(req: Request, res: Response) {
+    try {
+      const list = await this.blogService.getAllDeletedTemporaryBlogs()
+      res.status(200).json({
+        status: 200,
+        message: 'Get all temporary blog successfully',
+        data: classToPlain(list)
+      })
+      return
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({
+          status: 400,
+          message: error.message,
+          data: {}
+        })
+        return
+      }
+    }
+  }
+  async updateBlog(req: Request, res: Response) {
+    try {
+      const id = req.params.id
+      const { title, body } = req.body
+      const image = req.file?.filename as string
+      const inputDto = new UpdateBlogInputDto()
+      inputDto.title = title
+      inputDto.body = body
+      inputDto.image = image
+      inputDto.id = id
+      const response = await this.blogService.updateBlog(inputDto)
+      res.status(200).json({
+        status: 200,
+        message: 'Update blog successfully',
+        data: classToPlain(response)
+      })
+      return
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({
+          status: 400,
+          message: error.message,
+          data: {}
+        })
+        return
+      }
     }
   }
 }
