b094a0f084f69bb90851f71dbdca7e13afa953d7
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-05 17:00:15 +0700

    thêm route cho các chức năng cho recycle bin

--- src/routes/v1/blog.route.ts
+++ src/routes/v1/blog.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import BlogRepoImpl from '../../modules/blog/repo/BlogRepoImpl'
 import BlogServiceImpl from '../../modules/blog/service/BlogServiceImpl'
 import BlogController from '../../modules/blog/blog.controller'
+import { uploadBlogs } from '../../middleware/uploads'
 const router = Router()
 
 const repo = new BlogRepoImpl()
@@ -9,7 +10,19 @@ const service = new BlogServiceImpl(repo)
 const controller = new BlogController(service)
 
 router.get('/', controller.getAllBlog.bind(controller))
-router.post('/', controller.createBlog.bind(controller))
+router.get('/temporary', controller.getAllTemporaryBlogs.bind(controller))
+router.post(
+  '/',
+  uploadBlogs.single('image'),
+  controller.createBlog.bind(controller)
+)
 router.get('/:id', controller.getDetailBlog.bind(controller))
 router.delete('/', controller.deleteBlog.bind(controller))
+router.delete('/temporary', controller.deleteTemporaryBlogs.bind(controller))
+router.put('/temporary', controller.restoreTemporaryBlogs.bind(controller))
+router.put(
+  '/:id',
+  uploadBlogs.single('image'),
+  controller.updateBlog.bind(controller)
+)
 export const blogRouter = router
