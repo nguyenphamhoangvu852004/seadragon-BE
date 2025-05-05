25b602ac1b9f574b4dd497c6a49e90b09c6d3115
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-05 14:42:01 +0700

    thêm xoá mềm

--- src/routes/v1/customer.route.ts
+++ src/routes/v1/customer.route.ts
@@ -14,15 +14,19 @@ const service = new CustomerServiceImpl(repo)
 const controller = new CustomerController(service)
 
 router.get('/', controller.getAllCustomers.bind(controller))
+router.get(
+  '/temporary',
+  controller.getAllDeletedTemporaryCustomers.bind(controller)
+)
 router.get('/:id', controller.getDetailCustomers.bind(controller))
 router.post('/', controller.createCustomer.bind(controller))
 router.delete(
   '/temporary',
   controller.deleteTemporaryCustomers.bind(controller)
 )
+router.delete('/', controller.deleteCustomers.bind(controller))
 router.put(
   '/temporary/:id',
-  controller.restoreTemporaryCustomers.bind(controller)
+  controller.restoreTemporaryCustomer.bind(controller)
 )
-router.delete('/', controller.deleteCustomers.bind(controller))
 export const customerRouter = router
