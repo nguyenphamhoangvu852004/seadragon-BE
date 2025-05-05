ec4c76681113ae240881bab8a12562667cdaa7a5
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-05 16:56:35 +0700

    config upload for blogs

--- src/middleware/uploads.ts
+++ src/middleware/uploads.ts
@@ -1,17 +1,32 @@
 import multer from 'multer'
 import path from 'path'
 
-// Cấu hình lưu file
 const storageProducts = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/products') // thư mục lưu
+    cb(null, 'uploads/products')
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-    const ext = path.extname(file.originalname) // lấy đuôi file .jpg/.png
+    const ext = path.extname(file.originalname)
     cb(null, file.fieldname + '-' + uniqueSuffix + ext)
   }
 })
 
+const storageBlogs = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/blogs')
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    const ext = path.extname(file.originalname)
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+  }
+})
 // Khởi tạo multer
-export const uploadProducts = multer({ storage: storageProducts })
+export const uploadProducts = multer({
+  storage: storageProducts
+})
+
+export const uploadBlogs = multer({
+  storage: storageBlogs
+})
