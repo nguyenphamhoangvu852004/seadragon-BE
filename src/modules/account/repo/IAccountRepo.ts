609f317406159f59bfbe1f3b96078ce8dc3356b3
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-06 14:43:41 +0700

    add setRole interface

--- src/modules/account/repo/IAccountRepo.ts
+++ src/modules/account/repo/IAccountRepo.ts
@@ -1,7 +1,10 @@
+import { Accounts } from '../../../entities/accounts.entity'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default interface IAccountRepo {
   checkAccountExistByEmail(accountId: string): Promise<boolean>
   createAccount(account: any): Promise<any>
   findAccount(data: any): Promise<any>
-  getList():Promise<any>
+  getList(): Promise<any>
+  setRolesToAccount(userId: number, roles: string[]): Promise<Accounts>
 }
