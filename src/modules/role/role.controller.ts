ab1415941e4bc18d3d3316b4070967fa95da3d5f
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-06 11:21:54 +0700

    add controller

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,1 +1,7 @@
+import IRoleService from "./service/IRoleService"
 
+export default class RoleController {
+  roleService: IRoleService
+  constructor(roleService: IRoleService) {
+    this.roleService = roleService  }
+}
