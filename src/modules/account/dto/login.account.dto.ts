5b6d64259db6e519b699b425747da5b6cca8ff2e
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-06 14:43:17 +0700

    create dto for payload

--- src/modules/account/dto/login.account.dto.ts
+++ src/modules/account/dto/login.account.dto.ts
@@ -3,8 +3,25 @@ export default class LoginAccountDTO {
   password!: string
 }
 
+export class UserPayload {
+  id!: string
+  email!: string
+  username!: string
+  roles!: RolePayload[]
+  constructor(data?: Partial<UserPayload>) {
+    Object.assign(this, data)
+  }
+}
+export class RolePayload {
+  id!: string
+  name!: string
+  constructor(data?: Partial<RolePayload>) {
+    Object.assign(this, data)
+  }
+}
+
 export class LoginAccountOutputDTO {
-    id!: string
+  id!: string
   accessToken!: string
   refreshToken!: string
 }
