f80c7df8928cfb2edb82854dd9159b5701c2171e
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-08 20:51:24 +0700

    config env

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config()
 export const env = {
   PORT: Number(process.env.PORT),
   DB_HOST: process.env.DB_HOST,
@@ -10,14 +11,14 @@ export const env = {
   DB_LOGGING: process.env.DB_LOGGING,
   CORS_ORIGIN: process.env.CORS_ORIGIN,
 
-  INIT_ADMIN_USERNAME: process.env.INITIAL_ADMIN_USERNAME as string,
-  INIT_ADMIN_EMAIL: process.env.INITIAL_ADMIN_EMAIL as string,
-  INIT_ADMIN_PASSWORD: process.env.INITIAL_ADMIN_PASSWORD as string,
+  INIT_ADMIN_USERNAME: process.env.INITIAL_ADMIN_USERNAME,
+  INIT_ADMIN_EMAIL: process.env.INITIAL_ADMIN_EMAIL,
+  INIT_ADMIN_PASSWORD: process.env.INITIAL_ADMIN_PASSWORD,
 
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
-  ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
-  REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES_IN: Number(process.env.ACCESS_TOKEN_EXPIRES_IN),
+  REFRESH_TOKEN_EXPIRES_IN: Number(process.env.REFRESH_TOKEN_EXPIRES_IN),
 
   MAIL_HOST: process.env.MAIL_HOST,
   MAIL_PORT: Number(process.env.MAIL_PORT),
@@ -25,3 +26,4 @@ export const env = {
   MAIL_PASS: process.env.MAIL_PASS,
   MAIL_ADMIN_RECEIVER: process.env.MAIL_ADMIN_RECEIVER
 }
+
